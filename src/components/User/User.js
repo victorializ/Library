ce12e854442f0018429f97fee409412d73a71903
Viktoriia Lizeikina <vlize@softserveinc.com>
2020-12-21 23:37:36 +0200

    fix styles

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -7,7 +7,7 @@ import { colors } from "../../assets/semantic-colors";
 
 const User = ({firstName, lastName}) => 
     firstName && lastName ?      
-        <Segment color={colors.primary} className='user' as={Link} to='/orders'>
+        <Segment color={colors.primary} className='user current__user' as={Link} to='/orders'>
             <Icon name="user outline" />
             <div className='user__name'>
                 {firstName} {lastName}
